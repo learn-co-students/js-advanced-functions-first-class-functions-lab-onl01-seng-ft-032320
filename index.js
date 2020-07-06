88137c940006ab709fbbf4e0e50613658c5e63b6
ferrisbueller66 <github email address>
2020-07-06 19:53:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+    let firstTwo = []
+    for (let i = 0; i < 2; i++) {
+        firstTwo.push(drivers[i]);
+      }
+     return firstTwo
+}
+
+const returnLastTwoDrivers = function(drivers){
+    let newArray = drivers.slice(2);
+    return newArray;
+ }
+
+ const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+  // function createFareMultiplier(intg) {
+  //   if (intg === 2) {
+  //     return fareDoubler(intg)
+  //   }
+  //   else if (intg === 3) {
+  //     return fareTripler(intg)
+  //   }
+    
+  // }
+
+    // const fareDoubler = (fare)
+  //   return fare * 2
+  // }
+  // const fareTripler = function(fare){
+  //   return fare * 3
+  // }
+                                        
+  const createFareMultiplier = function(multiply) {
+    return function(value) {
+      return multiply * value;
+    }
+  }
+
+  const fareDoubler = createFareMultiplier(2);
+  const fareTripler = createFareMultiplier(3);
+
+
+
+  const selectDifferentDrivers = function(drivers, randomMethod){
+    return randomMethod(drivers);
+  }
