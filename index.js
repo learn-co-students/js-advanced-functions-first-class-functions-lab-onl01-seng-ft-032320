778ab10f916c8f17b3aa6bde57d2be5d16dd9431
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2020-07-06 14:40:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => (drivers.slice(0,2));
+
+const returnLastTwoDrivers = (drivers) => (drivers.slice(-2));
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (multiplier) => ((fare) => (fare * multiplier));
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+let randomFunc = selectingDrivers[Math.round(Math.random())]
+
+const selectDifferentDrivers = (drivers, randomFunc) => (randomFunc(drivers))
